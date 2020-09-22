8051c41c2d38746fd00946b81872999a5651788b
duch0416 <mikolajduchewicz@gmail.com>
2020-09-22 21:26:34 +0200

    fix

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import {Header} from "../styles/title"
+import Header from "./Header"
 import { headertxt } from "../data/text"
 import Hero from "./Hero"
 import Logo from "./Logo"
